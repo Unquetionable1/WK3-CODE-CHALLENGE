c21c7073e976e0122ec6105749494f51b73f135f
Edmound <eddnjoori@gmail.com>
2024-04-09 01:48:38 +0300

    Adds responsive DOM elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,67 @@
-async function logMovies() {
-  const response = await fetch("http://localhost:3000/films");
-  const movies = await response.json();
-}
+// Your code here
+document.addEventListener('DOMContentLoaded',()=>{
+    const lists= document.querySelector('ul')
+    lists.textContent=""
+    const buyTicket= document.getElementById('buy-ticket')
+    
+    fetch("http://localhost:3000/films")
+    .then(response => {
+      if(!response.ok){
+        throw new Error('Could not fetch resource')
+      }
+      return response.json()
+    })
+    .then(data => {
+      data.forEach(movie => {
+        const li = document.createElement('li')
+        li.innerHTML +=`${movie.title}`
+        lists.appendChild(li)
+        li.addEventListener('click',(e)=>{
+            displayDetails(movie)
+        })
+       
+        buyTicket.addEventListener('click',(e)=>{
+            updateTickets(movie)
+        })
+      });
+    })
+    .catch(error => console.error())
+  
+  })
 
 
+  function displayDetails(movie){
+    const title= document.getElementById('title')
+    title.textContent=movie.title
 
+    const runtime = document.getElementById('runtime')
+    runtime.innerHTML=`<p>runtime: ${movie.runtime}</p>`
+
+    const showtime = document.getElementById('showtime')
+    showtime.innerHTML=`<p>runtime: ${movie.showtime}</p>`
+
+    const poster = document.getElementById('poster')
+    poster.src=`${movie.poster}`
+    poster.alt=`${movie.title}`
+    
+    const description =document.getElementById('film-info')
+    description.innerHTML=`<p>${movie.description}</p>`
+
+    const remaining=document.getElementById('ticket-num')
+    remaining.innerHTML=`${movie.capacity}-${movie.sold} tickets`
+    }
+
+    function updateTickets(movie){
+        if (movie.tickets_sold >= movie.capacity) {
+            buyTicket.textContent = "Sold Out";
+            buyTicket.setAttribute("disabled", "true");
+        } else {
+            buyTicket.textContent = "Buy Ticket";
+            buyTicket.removeAttribute("disabled");
+        }
+        if (currentMovie.tickets_sold < currentMovie.capacity) {
+            // Update the sold tickets count
+            movie.tickets_sold++;
+           
+        }
+    }
